25f38a9bd0eaa31a9e25de0f44a2525c0be0e985
Alexandr <apower.ru@mail.ru>
2022-04-14 00:09:40 +0300

    Work

--- mysite/singlepage/models.py
+++ mysite/singlepage/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Table(models.Model):
+    """
+    date - дата
+    name - название
+    amount - количество
+    distance - расстояние
+    """
+
+    date = models.DateField()
+    name = models.CharField(max_length=255)
+    amount = models.PositiveIntegerField()
+    distance = models.PositiveIntegerField()
--- mysite/singlepage/views.py
+++ mysite/singlepage/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
-
+from . models import Table
 
 # Create your views here.
 def index(request):
     return render(request, "singlepage/index.html")
 
 
-texts = ["111",
+texts = [Table.date,
          "222",
          "333",
          "444", ]
@@ -18,3 +18,7 @@ def section(request, num):
         return HttpResponse(texts[num - 1])
     else:
         raise Http404("No such section")
+
+def ServiceListView(request):
+    serv_list= Table.objects.all().order_by('name')
+    returnt render (request, 'services.html', {'serv_list':serv_list})
