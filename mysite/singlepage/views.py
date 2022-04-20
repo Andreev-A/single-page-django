5fc6a62f77c527aaeb53f96c63e2294e31472610
Alexandr <apower.ru@mail.ru>
2022-04-20 20:44:50 +0300

    Rename vars

--- mysite/singlepage/views.py
+++ mysite/singlepage/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, Http404
 from .models import Table
 
 NUMBER_OF_TABLE_ROW = 7
-_database_data = []
+_dataset_from_database = []
 _page = 1
 
 
@@ -17,7 +17,7 @@ def fetch_from_database(form_data):
     Receiving and filtering data  from MySQL.
     """
     column_selection, condition_selection, value_to_filter = form_data
-    data = []
+    dataset = []
     if column_selection == 'name' and condition_selection in ('equal', 'contains'):
         queryset = Table.objects.filter(name=value_to_filter)
     elif column_selection == 'name' and condition_selection == 'larger':
@@ -38,29 +38,29 @@ def fetch_from_database(form_data):
         queryset = Table.objects.filter(distance__lt=value_to_filter)
     else:
         queryset = Table.objects.all()
-    for i in range(len(queryset)):
-        data.append(str(queryset[i]).split())
-    return data
+    for data in queryset:
+        dataset.append(str(data).split())
+    return dataset
 
 
-def table_body_output(data, page_navigation):
+def table_body_output(dataset, page_navigation):
     """
     Forming the table body in HTML page by page.
     """
     global _page
-    number_of_pages = len(data) // NUMBER_OF_TABLE_ROW + 1 \
-        if not data or len(data) % NUMBER_OF_TABLE_ROW else len(data) // NUMBER_OF_TABLE_ROW
+    number_of_pages = len(dataset) // NUMBER_OF_TABLE_ROW + 1 \
+        if not dataset or len(dataset) % NUMBER_OF_TABLE_ROW else len(dataset) // NUMBER_OF_TABLE_ROW
     if page_navigation == -1 and _page > 1:
         _page += page_navigation
     elif page_navigation == 1 and _page < number_of_pages:
         _page += page_navigation
     elif not page_navigation:
         _page = 1
-    result = []
+    table_rows = []
     start = NUMBER_OF_TABLE_ROW * _page - NUMBER_OF_TABLE_ROW
     stop = NUMBER_OF_TABLE_ROW * _page
-    for el in data[start:stop]:
-        date, name, amount, distance = el
+    for data in dataset[start:stop]:
+        date, name, amount, distance = data
         table_row = f"""
                      <tr class="table__row">
                      <td class="table__down">{date}</td>
@@ -68,28 +68,28 @@ def table_body_output(data, page_navigation):
                      <td class="table__down">{amount}</td>
                      <td class="table__down">{distance}</td></tr>
                      """
-        result += table_row
-    result += f"""
+        table_rows += table_row
+    table_rows += f"""
                <tr class="table__row">
                <th colspan="4">Страница {_page} из {number_of_pages}</th></tr>
                """
-    return result
+    return table_rows
 
 
 def post_list(request):
     """
     Getting form data. Return of the first page.
     """
-    global _database_data
+    global _dataset_from_database
     if request.method == "POST":
         column_selection = request.POST.get('column_selection')
         condition_selection = request.POST.get('condition_selection')
         value_to_filter = request.POST.get('value_to_filter')
         form_data = column_selection, condition_selection, value_to_filter
-        _database_data = fetch_from_database(form_data)
+        _dataset_from_database = fetch_from_database(form_data)
         page_navigation = 0
-        result = table_body_output(_database_data, page_navigation)
-        return HttpResponse(result)
+        table_rows = table_body_output(_dataset_from_database, page_navigation)
+        return HttpResponse(table_rows)
     else:
         raise Http404("Bad request")
 
@@ -98,17 +98,17 @@ def section(request, num):
     """
     Page navigation. Return the selected page.
     """
-    global _database_data
+    global _dataset_from_database
     if 1 <= num <= 3:
         page_navigation = 0
         if num == 1:
             form_data = (None, None, None)
-            _database_data = fetch_from_database(form_data)
+            _dataset_from_database = fetch_from_database(form_data)
         elif num == 2:
             page_navigation = -1
         elif num == 3:
             page_navigation = 1
-        result = table_body_output(_database_data, page_navigation)
-        return HttpResponse(result)
+        table_rows = table_body_output(_dataset_from_database, page_navigation)
+        return HttpResponse(table_rows)
     else:
         raise Http404("Bad request")
